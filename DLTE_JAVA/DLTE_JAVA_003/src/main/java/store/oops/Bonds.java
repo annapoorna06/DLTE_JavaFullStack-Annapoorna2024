15b7d27ff1f0d3d8bcc73a467d6c84b699c91a8f
annapoorna06 <annapoornapai6@gmail.com>
2024-02-21 18:06:39 +0530

    oops concepts

--- DLTE_JAVA/DLTE_JAVA_003/src/main/java/store/oops/Bonds.java
+++ DLTE_JAVA/DLTE_JAVA_003/src/main/java/store/oops/Bonds.java
@@ -12,13 +12,14 @@ public class Bonds {
     private double interestRate;
     private String taxStatus;
     private Integer period;
+    private String bondName;
 
     public static void main(String[] args) {
-        Bonds Government_bond = new Bonds(4, 7.2, "no tax", 5);
-        Bonds High_yield_Bonds = new Bonds(5, 6.2, "no tax", 5);
-        Bonds Mortgage_Backed_Securities = new Bonds(5, 5.7, "no tax", 5);
-        Bonds Zero_Coupon_Bonds = new Bonds(5, 7.1, "no tax", 10);
-        Bonds Callable_Bonds = new Bonds(5, 5.1, "no tax", 8);
+        Bonds Government_bond = new Bonds(4, 7.2, "no tax", 5,"Treasury Bills");
+        Bonds High_yield_Bonds = new Bonds(5, 6.2, "no tax", 5, "investment-grade");
+        Bonds Mortgage_Backed_Securities = new Bonds(5, 5.7, "no tax", 5,"Pass-through MBS");
+        Bonds Zero_Coupon_Bonds = new Bonds(5, 7.1, "no tax", 10,"Zero Coupon Bonds");
+        Bonds Callable_Bonds = new Bonds(5, 5.1, "no tax", 8,"redeemable bonds");
         Bonds bonds[] = {Government_bond, High_yield_Bonds, Mortgage_Backed_Securities, Zero_Coupon_Bonds, Callable_Bonds};
     }
 
