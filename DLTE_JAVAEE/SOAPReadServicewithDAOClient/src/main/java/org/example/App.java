8e3e1995c2ee1a1f5faf0ec517be2970e60f7359
annapoorna06 <annapoornapai6@gmail.com>
2024-03-18 20:17:22 +0530

    SOAP read service with DAO

--- DLTE_JAVAEE/SOAPReadServicewithDAOClient/src/main/java/org/example/App.java
+++ DLTE_JAVAEE/SOAPReadServicewithDAOClient/src/main/java/org/example/App.java
@@ -1,13 +1,16 @@
 package org.example;
+import service.TransactionByUsername;
+import service.TransactionByUsernameService;
+import service.Transactions;
+import java.util.List;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        TransactionByUsernameService transactionByUsernameService=new TransactionByUsernameService();
+        TransactionByUsername transactionByUsername=transactionByUsernameService.getTransactionByUsernamePort();
+        List<Transactions> transactions = (List<Transactions>) transactionByUsername.findAllByUsers("annapoornapai");
+        for (Transactions each : transactions) {
+            System.out.println(each);
+        }
     }
 }
