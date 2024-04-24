bdfd54f2b18f681f9add1c32a8b95b66ca961ce0
annapoorna06 <annapoornapai6@gmail.com>
2024-04-24 10:13:12 +0530

    springboot thymeleaf

--- DLTE_Project/MyBank_Project/MyBank_WebService/src/main/java/project/backend/configuration/SoapPhase.java
+++ DLTE_Project/MyBank_Project/MyBank_WebService/src/main/java/project/backend/configuration/SoapPhase.java
@@ -1,69 +1,69 @@
-package project.backend.configuration;
-
-import com.sun.org.apache.xml.internal.utils.res.XResourceBundle;
-import mybank.dao.entity.LoansAvailable;
-import mybank.dao.exceptions.LoanServiceException;
-import mybank.dao.exceptions.NoLoanDataException;
-import mybank.dao.interfaces.LoansInterface;
-import mybank.dao.services.LoanServices;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.ws.server.endpoint.annotation.Endpoint;
-import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
-import org.springframework.ws.server.endpoint.annotation.RequestPayload;
-import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
-import services.loans.ServiceStatus;
-import services.loans.ViewAllAvailableLoanRequest;
-import services.loans.ViewAllAvailableLoanResponse;
-import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
-
-@ComponentScan("mybank.dao")
-@Endpoint
-public class SoapPhase {
-    @Autowired
-    public LoansInterface interfaceServices;
-    Logger logger= LoggerFactory.getLogger(LoanServices.class);
-
-    private final String url = "http://loans.services";
-    ResourceBundle resourceBundle=ResourceBundle.getBundle("application");
-    //service to fetch all the available loans
-    @PayloadRoot(namespace = url, localPart = "viewAllAvailableLoanRequest")
-    @ResponsePayload
-    public ViewAllAvailableLoanResponse viewAvailLoanRequest(@RequestPayload ViewAllAvailableLoanRequest request){
-        ViewAllAvailableLoanResponse response=new ViewAllAvailableLoanResponse();
-        ServiceStatus serviceStatus=new ServiceStatus();
-        try {
-            List<LoansAvailable> allLoansDao = interfaceServices.allAvailableLoans();
-            List<services.loans.LoanAvailable> allLoans=new ArrayList<>();
-            //java8 features
-            allLoansDao.forEach(each->{
-                services.loans.LoanAvailable currentLoan=new services.loans.LoanAvailable();
-                BeanUtils.copyProperties(each,currentLoan);
-                allLoans.add(currentLoan);
-            });
-            //sets https status code-200
-            serviceStatus.setStatus(HttpServletResponse.SC_OK);
-            response.getLoanAvailable().addAll(allLoans);
-            logger.info(resourceBundle.getString("loan.server.available"));
-        }catch (LoanServiceException exception){
-            //sets https status code-500
-            serviceStatus.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            serviceStatus.setMessage(exception.toString());
-            logger.info(resourceBundle.getString("loan.server.error"));
-        }
-        catch (NoLoanDataException exception){
-            //sets https status code-500
-            serviceStatus.setStatus(HttpServletResponse.SC_NOT_FOUND);
-            serviceStatus.setMessage(exception.toString());
-            logger.info(resourceBundle.getString("loan.server.error"));
-        }
-        response.setServiceStatus(serviceStatus);
-        return response;
-    }
-}
+//package project.backend.configuration;
+//
+//import com.sun.org.apache.xml.internal.utils.res.XResourceBundle;
+//import mybank.dao.entity.LoansAvailable;
+//import mybank.dao.exceptions.LoanServiceException;
+//import mybank.dao.exceptions.NoLoanDataException;
+//import mybank.dao.interfaces.LoansInterface;
+//import mybank.dao.services.LoanServices;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.BeanUtils;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.ws.server.endpoint.annotation.Endpoint;
+//import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+//import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+//import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+//import services.loans.ServiceStatus;
+//import services.loans.ViewAllAvailableLoanRequest;
+//import services.loans.ViewAllAvailableLoanResponse;
+//import javax.servlet.http.HttpServletResponse;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.ResourceBundle;
+//
+//@ComponentScan("mybank.dao")
+//@Endpoint
+//public class SoapPhase {
+//    @Autowired
+//    public LoansInterface interfaceServices;
+//    Logger logger= LoggerFactory.getLogger(LoanServices.class);
+//
+//    private final String url = "http://loans.services";
+//    ResourceBundle resourceBundle=ResourceBundle.getBundle("application");
+//    //service to fetch all the available loans
+//    @PayloadRoot(namespace = url, localPart = "viewAllAvailableLoanRequest")
+//    @ResponsePayload
+//    public ViewAllAvailableLoanResponse viewAvailLoanRequest(@RequestPayload ViewAllAvailableLoanRequest request){
+//        ViewAllAvailableLoanResponse response=new ViewAllAvailableLoanResponse();
+//        ServiceStatus serviceStatus=new ServiceStatus();
+//        try {
+//            List<LoansAvailable> allLoansDao = interfaceServices.allAvailableLoans();
+//            List<services.loans.LoanAvailable> allLoans=new ArrayList<>();
+//            //java8 features
+//            allLoansDao.forEach(each->{
+//                services.loans.LoanAvailable currentLoan=new services.loans.LoanAvailable();
+//                BeanUtils.copyProperties(each,currentLoan);
+//                allLoans.add(currentLoan);
+//            });
+//            //sets https status code-200
+//            serviceStatus.setStatus(HttpServletResponse.SC_OK);
+//            response.getLoanAvailable().addAll(allLoans);
+//            logger.info(resourceBundle.getString("loan.server.available"));
+//        }catch (LoanServiceException exception){
+//            //sets https status code-500
+//            serviceStatus.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+//            serviceStatus.setMessage(exception.toString());
+//            logger.info(resourceBundle.getString("loan.server.error"));
+//        }
+//        catch (NoLoanDataException exception){
+//            //sets https status code-500
+//            serviceStatus.setStatus(HttpServletResponse.SC_NOT_FOUND);
+//            serviceStatus.setMessage(exception.toString());
+//            logger.info(resourceBundle.getString("loan.server.error"));
+//        }
+//        response.setServiceStatus(serviceStatus);
+//        return response;
+//    }
+//}
--- DLTE_Project/MyBank_Project/MyBank_WebService/src/main/java/project/backend/rest/LoanServicesController.java
+++ DLTE_Project/MyBank_Project/MyBank_WebService/src/main/java/project/backend/rest/LoanServicesController.java
@@ -1,72 +1,72 @@
-package project.backend.rest;
-import mybank.dao.entity.LoansAvailable;
-import mybank.dao.exceptions.LoanServiceException;
-import mybank.dao.exceptions.NoLoanDataException;
-import mybank.dao.interfaces.LoansInterface;
-import mybank.dao.services.LoanServices;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletResponse;
-import java.util.List;
-import java.util.ResourceBundle;
-
-@RestController
-@RequestMapping("/loans")
-public class LoanServicesController {
-
-    Logger logger = LoggerFactory.getLogger(LoanServices.class);
-    ResourceBundle resourceBundle = ResourceBundle.getBundle("application");
-    @Autowired
-    private LoansInterface loanService;
-    //http://localhost:8082/loans/{LoanType}
-    @GetMapping("/{loanType}")
-    public ResponseEntity<Object> findByLoanType(@PathVariable String loanType, HttpServletResponse response) {
-        try {
-            List<LoansAvailable> loans = loanService.findByLoanType(loanType);
-            if (loans.isEmpty()) {
-                return ResponseEntity.notFound().build();
-            } else {
-                return ResponseEntity.ok(loans);
-            }
-        } catch (NoLoanDataException e) {
-            response.setStatus(HttpServletResponse.SC_NO_CONTENT);//204 no content
-            logger.error(resourceBundle.getString("no.loanType"), e);
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        } catch (LoanServiceException e) {
-            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);//500 no content
-            logger.error(resourceBundle.getString("db.error"), e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
-        }
-    }
-
-    @GetMapping("/{loanType}/emi")
-    public double calculateEMI(@PathVariable String loanType,
-                               @RequestParam double amount,
-                               @RequestParam int tenure,
-                               HttpServletResponse response) throws LoanServiceException {
-       // http://localhost:8082/loans/Gold/emi?amount=10000&tenure=12
-        try {
-            // Retrieve rate of interest from database based on loan type
-            double rateOfInterest = loanService.getRateOfInterestByLoanType(loanType);
-            // Convert rate of interest to decimal
-            double monthlyInterest = rateOfInterest / (12 * 100);
-            // Calculate EMI
-            double emi = (amount * monthlyInterest * Math.pow(1 + monthlyInterest, tenure)) / (Math.pow(1 + monthlyInterest, tenure) - 1);
-            //return resourceBundle.getString("emi.pay") + loanType + " is:" + emi;
-            return emi;
-        } catch (DataAccessException e) {
-            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);//500
-            logger.error(resourceBundle.getString("emi.calculation.error"));
-            throw new LoanServiceException(resourceBundle.getString("emi.calculation.error"));
-        }
-    }
-}
-
-
-//rest API document-http://localhost:8082/v3/api-docs
+//package project.backend.rest;
+//import mybank.dao.entity.LoansAvailable;
+//import mybank.dao.exceptions.LoanServiceException;
+//import mybank.dao.exceptions.NoLoanDataException;
+//import mybank.dao.interfaces.LoansInterface;
+//import mybank.dao.services.LoanServices;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.dao.DataAccessException;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import javax.servlet.http.HttpServletResponse;
+//import java.util.List;
+//import java.util.ResourceBundle;
+//
+//@RestController
+//@RequestMapping("/loans")
+//public class LoanServicesController {
+//
+//    Logger logger = LoggerFactory.getLogger(LoanServices.class);
+//    ResourceBundle resourceBundle = ResourceBundle.getBundle("application");
+//    @Autowired
+//    private LoansInterface loanService;
+//    //http://localhost:8082/loans/{LoanType}
+//    @GetMapping("/{loanType}")
+//    public ResponseEntity<Object> findByLoanType(@PathVariable String loanType, HttpServletResponse response) {
+//        try {
+//            List<LoansAvailable> loans = loanService.findByLoanType(loanType);
+//            if (loans.isEmpty()) {
+//                return ResponseEntity.notFound().build();
+//            } else {
+//                return ResponseEntity.ok(loans);
+//            }
+//        } catch (NoLoanDataException e) {
+//            response.setStatus(HttpServletResponse.SC_NO_CONTENT);//204 no content
+//            logger.error(resourceBundle.getString("no.loanType"), e);
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+//        } catch (LoanServiceException e) {
+//            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);//500 no content
+//            logger.error(resourceBundle.getString("db.error"), e);
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+//        }
+//    }
+//
+//    @GetMapping("/{loanType}/emi")
+//    public double calculateEMI(@PathVariable String loanType,
+//                               @RequestParam double amount,
+//                               @RequestParam int tenure,
+//                               HttpServletResponse response) throws LoanServiceException {
+//       // http://localhost:8082/loans/Gold/emi?amount=10000&tenure=12
+//        try {
+//            // Retrieve rate of interest from database based on loan type
+//            double rateOfInterest = loanService.getRateOfInterestByLoanType(loanType);
+//            // Convert rate of interest to decimal
+//            double monthlyInterest = rateOfInterest / (12 * 100);
+//            // Calculate EMI
+//            double emi = (amount * monthlyInterest * Math.pow(1 + monthlyInterest, tenure)) / (Math.pow(1 + monthlyInterest, tenure) - 1);
+//            //return resourceBundle.getString("emi.pay") + loanType + " is:" + emi;
+//            return emi;
+//        } catch (DataAccessException e) {
+//            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);//500
+//            logger.error(resourceBundle.getString("emi.calculation.error"));
+//            throw new LoanServiceException(resourceBundle.getString("emi.calculation.error"));
+//        }
+//    }
+//}
+//
+//
+////rest API document-http://localhost:8082/v3/api-docs
